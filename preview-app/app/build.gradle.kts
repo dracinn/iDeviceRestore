9303e07917742ae73abdf3a3b74a6b3e6724b78f
dracinn <dracinn@gmail.com>
2026-09-08 22:38:15 -0500

    Add preview app module build file

---
+++ preview-app/app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.idevicerestore.preview"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.idevicerestore.preview"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0-preview"
+    }
+}
